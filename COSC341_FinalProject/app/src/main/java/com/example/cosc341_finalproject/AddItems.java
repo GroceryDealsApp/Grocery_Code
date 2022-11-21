418f89e4c0fc2f3dfd7900a0c26e9e366e1114eb
kitkat6410 <kjbarnes6410@gmail.com>
2022-11-20 18:05:57 -0800

    Added activities

---
+++ COSC341_FinalProject/app/src/main/java/com/example/cosc341_finalproject/AddItems.java
@@ -0,0 +1,14 @@
+package com.example.cosc341_finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddItems extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_items);
+    }
+}
---
+++ COSC341_FinalProject/app/src/main/java/com/example/cosc341_finalproject/StoreComparison.java
@@ -0,0 +1,14 @@
+package com.example.cosc341_finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StoreComparison extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_comparison);
+    }
+}
