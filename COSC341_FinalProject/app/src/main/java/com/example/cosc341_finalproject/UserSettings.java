475893d7ebca02b2464369127c8297572a6d07f7
adamf <adamfipke@gmail.com>
2022-11-29 22:29:32 -0800

    made user settings screen

--- COSC341_FinalProject/app/src/main/java/com/example/cosc341_finalproject/UserSettings.java
+++ COSC341_FinalProject/app/src/main/java/com/example/cosc341_finalproject/UserSettings.java
@@ -2,8 +2,10 @@ package com.example.cosc341_finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class UserSettings extends AppCompatActivity {
 
@@ -15,4 +17,17 @@ public class UserSettings extends AppCompatActivity {
     public void onDone(View v){
         finish();
     }
+    public void onFavouriteItems(View v){
+        Intent intent = new Intent(this, FavouriteItem.class);
+        startActivity(intent);
+    }
+    public void onBlacklistedItems(View v){
+        Button b = findViewById(R.id.buttonBlacklistedItems);
+        b.setText("Not implemented yet");
+    }
+    public void onNotificationSettings(View v){
+        Intent intent = new Intent(this, FavItemNotification.class);
+        startActivity(intent);
+    }
+
 }
